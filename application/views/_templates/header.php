cb8a1327e7914bedf7eb9a1afc0253d74db9ac1f
Valentin_Lapointe <valentinlapointe99@gmail.com>
2020-04-22 19:38:28 +0200

    feat: ajout du dossier template avec le header et le footer de chaque page

---
+++ application/views/_templates/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Confinéma</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container">
+
+    <div class="row">
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+<!--                    <li class="nav-item active">-->
+<!--                        <a class="nav-link" href="--><?PHP //echo base_url("Accueil");?><!--">Accueil <span class="sr-only">(current)</span></a>-->
+<!--                    </li>-->
+<!--                    <li class="nav-item">-->
+<!--                        <a class="nav-link" href="--><?PHP //echo base_url("Historique");?><!--">Historique</a>-->
+<!--                    </li>-->
+<!--                    <li class="nav-item">-->
+<!--                        <a class="nav-link" href="--><?PHP //echo base_url("Graphique");?><!--">Graphique</a>-->
+<!--                    </li>-->
+<!--                    <li class="nav-item">-->
+<!--                        <a class="nav-link" href="--><?PHP //echo base_url("Tableau");?><!--">Tableau</a>-->
+<!--                    </li>-->
+<!--                    <li class="nav-item">-->
+<!--                        <a class="nav-link" href="--><?PHP //echo base_url("Parametre");?><!--">Parametre</a>-->
+<!--                    </li>-->
+                </ul>
+            </div>
+        </nav>
+
+    </div>
+
+</div>
---
+++ application/views/_templates/template.php
@@ -0,0 +1,10 @@
+<?PHP
+
+// Chargement du header
+$this->load->view('_templates/header.php');
+
+// Chargement du contenu
+$this->load->view($main_content);
+
+// Chargement du footer
+$this->load->view('_templates/footer.php');
