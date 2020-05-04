ac81157f597b6c0a5febecf11def633630792135
Valentin_Lapointe <valentinlapointe99@gmail.com>
2020-05-04 08:15:17 +0200

    fix: ameliore le front de la page d'inscription

--- application/views/Inscription/index.php
+++ application/views/Inscription/index.php
@@ -20,77 +20,82 @@ if(isset($verify)) :
 endif;
 ?>
 
-<form action="" method="POST">
-    <div class="form-group row">
-        <label for="inputEmail3" class="col-sm-2 col-form-label">Nom : </label>
-        <div class="col-sm-5">
-            <input type="text" class="form-control" name="nom" id="nom" value="<?PHP if(!isset($insert)) echo set_value('nom'); ?>">
+<div class="container mt-5">
+    <form action="" method="POST">
+        <h1 class="text-center mb-5">CONFINEMA</h1>
+        <div class="form-group row">
+            <label for="inputEmail3" class="col-sm-2 col-form-label">Nom : </label>
+            <div class="col-sm-7">
+                <input type="text" class="form-control" name="nom" id="nom" value="<?PHP if(!isset($insert)) echo set_value('nom'); ?>">
+            </div>
+            <?php echo form_error('nom', '<label for="nom" generated="true" class="error" style="display: block;">', '</label>'); ?>
         </div>
-        <?php echo form_error('nom', '<label for="nom" generated="true" class="error" style="display: block;">', '</label>'); ?>
-    </div>
 
-    <div class="form-group row">
-        <label for="inputEmail3" class="col-sm-2 col-form-label">Prénom : </label>
-        <div class="col-sm-5">
-            <input type="text" class="form-control" name="prenom" id="prenom" value="<?PHP if(!isset($insert)) echo set_value('prenom'); ?>">
+        <div class="form-group row">
+            <label for="inputEmail3" class="col-sm-2 col-form-label">Prénom : </label>
+            <div class="col-sm-7">
+                <input type="text" class="form-control" name="prenom" id="prenom" value="<?PHP if(!isset($insert)) echo set_value('prenom'); ?>">
+            </div>
+            <?php echo form_error('prenom', '<label for="prenom" generated="true" class="error" style="display: block;">', '</label>'); ?>
         </div>
-        <?php echo form_error('prenom', '<label for="prenom" generated="true" class="error" style="display: block;">', '</label>'); ?>
-    </div>
 
-    <div class="form-group row">
-        <label for="inputPassword3" class="col-sm-2 col-form-label">Email :</label>
-        <div class="col-sm-5">
-            <input type="email" class="form-control" name="email" id="email" value="<?PHP if(!isset($insert)) echo set_value('email'); ?>">
+        <div class="form-group row">
+            <label for="inputPassword3" class="col-sm-2 col-form-label">Email :</label>
+            <div class="col-sm-7">
+                <input type="email" class="form-control" name="email" id="email" value="<?PHP if(!isset($insert)) echo set_value('email'); ?>">
+            </div>
+            <?php echo form_error('email', '<label for="email" generated="true" class="error" style="display: block;">', '</label>'); ?>
         </div>
-        <?php echo form_error('email', '<label for="email" generated="true" class="error" style="display: block;">', '</label>'); ?>
-    </div>
 
-    <div class="form-group row">
-        <label for="inputPassword3" class="col-sm-2 col-form-label">Mot de passe :</label>
-        <div class="col-sm-5">
-            <input type="password" class="form-control" name="password" id="password">
+        <div class="form-group row">
+            <label for="inputPassword3" class="col-sm-2 col-form-label">Mot de passe :</label>
+            <div class="col-sm-7">
+                <input type="password" class="form-control" name="password" id="password">
+            </div>
+            <?php echo form_error('password', '<label for="password" generated="true" class="error" style="display: block;">', '</label>'); ?>
         </div>
-        <?php echo form_error('password', '<label for="password" generated="true" class="error" style="display: block;">', '</label>'); ?>
-    </div>
 
-    <div class="form-group row">
-        <label for="inputPassword3" class="col-sm-2 col-form-label">Confirmer mot de passe :</label>
-        <div class="col-sm-5">
-            <input type="password" class="form-control" name="password_verify" id="password_verify">
+        <div class="form-group row">
+            <label for="inputPassword3" class="col-sm-2 col-form-label">Confirmer mot de passe :</label>
+            <div class="col-sm-7">
+                <input type="password" class="form-control" name="password_verify" id="password_verify">
+            </div>
+            <?php echo form_error('password_verify', '<label for="password_verify" generated="true" class="error" style="display: block;">', '</label>'); ?>
         </div>
-        <?php echo form_error('password_verify', '<label for="password_verify" generated="true" class="error" style="display: block;">', '</label>'); ?>
-    </div>
 
-    <div class="form-group row">
-        <label for="inputEmail3" class="col-sm-2 col-form-label">Adresse : </label>
-        <div class="col-sm-5">
-            <input type="text" class="form-control" name="adresse" id="adresse" value="<?PHP if(!isset($insert)) echo set_value('adresse'); ?>">
+        <div class="form-group row">
+            <label for="inputEmail3" class="col-sm-2 col-form-label">Adresse : </label>
+            <div class="col-sm-7">
+                <input type="text" class="form-control" name="adresse" id="adresse" value="<?PHP if(!isset($insert)) echo set_value('adresse'); ?>">
+            </div>
+            <?php echo form_error('adresse', '<label for="adresse" generated="true" class="error" style="display: block;">', '</label>'); ?>
         </div>
-        <?php echo form_error('adresse', '<label for="adresse" generated="true" class="error" style="display: block;">', '</label>'); ?>
-    </div>
 
-    <div class="form-group row">
-        <label for="inputEmail3" class="col-sm-2 col-form-label">Code Postal : </label>
-        <div class="col-sm-5">
-            <input type="text" class="form-control" name="cp" id="cp" value="<?PHP if(!isset($insert)) echo set_value('cp'); ?>">
+        <div class="form-group row">
+            <label for="inputEmail3" class="col-sm-2 col-form-label">Code Postal : </label>
+            <div class="col-sm-7">
+                <input type="text" class="form-control" name="cp" id="cp" value="<?PHP if(!isset($insert)) echo set_value('cp'); ?>">
+            </div>
+            <?php echo form_error('cp', '<label for="cp" generated="true" class="error" style="display: block;">', '</label>'); ?>
         </div>
-        <?php echo form_error('cp', '<label for="cp" generated="true" class="error" style="display: block;">', '</label>'); ?>
-    </div>
 
-    <div class="form-group row">
-        <label for="inputEmail3" class="col-sm-2 col-form-label">Ville : </label>
-        <div class="col-sm-5">
-            <input type="text" class="form-control" name="ville" id="ville" value="<?PHP if(!isset($insert)) echo set_value('ville'); ?>">
+        <div class="form-group row">
+            <label for="inputEmail3" class="col-sm-2 col-form-label">Ville : </label>
+            <div class="col-sm-7">
+                <input type="text" class="form-control" name="ville" id="ville" value="<?PHP if(!isset($insert)) echo set_value('ville'); ?>">
+            </div>
+            <?php echo form_error('ville', '<label for="ville" generated="true" class="error" style="display: block;">', '</label>'); ?>
         </div>
-        <?php echo form_error('ville', '<label for="ville" generated="true" class="error" style="display: block;">', '</label>'); ?>
-    </div>
 
-    <div class="form-group row">
-        <div class="col-sm-10">
-            <button type="submit" class="btn btn-primary">S'inscrire</button>
+        <div class="form-group row">
+            <div class="col-sm-10">
+                <button type="submit" class="btn btn-primary">S'inscrire</button>
+            </div>
         </div>
-    </div>
-</form>
+    </form>
+
+    <p><a href="<?PHP echo base_url("Connexion");?>">J'ai déjà un compte</a></p>
+</div>
 
 
-<p><a href="<?PHP echo base_url("Connexion");?>">J'ai déjà un compte</a></p>
+
